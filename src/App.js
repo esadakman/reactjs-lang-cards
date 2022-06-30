e7bbd90d20df2226b07906c22443e5f2dc81a6f2
esadakman <98649983+esadakman@users.noreply.github.com>
2022-06-30 09:50:31 +0300

    app js fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import Card from "./components/card/Card";
+ import Card from "./components/card/Card";
 import "./App.scss";
 import data from "./helper/data";
 import logoReact from "./assets/react.svg";
@@ -26,32 +25,3 @@ function App() {
 }
 
 export default App;
-=======
-import Card from "./components/card/Card";
-import "./App.scss";
-import data from "./helper/data";
-import logoReact from "./assets/react.svg";
-
-function App() {
-  return (
-    <>
-      <div className="App">
-        <div className="header">
-          <div className="logo">
-            <img src={logoReact} alt="" />
-          </div>
-        </div>
-        <div className="card">
-          <h2 className="title">Languages</h2>
-
-          {data.map((info, index) => {
-            return <Card key={index} {...info} />;
-          })}
-        </div>
-      </div>
-    </>
-  );
-}
-
-export default App;
->>>>>>> 08a2f3c0dc8c7844e8134e3b568e5551a1cd27c5
